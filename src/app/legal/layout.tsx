9148660b8d3cd525dacdd7f841af377ff5c2efcf
Inira Delgado <iniradelgado@gmail.com>
2025-07-30 04:47:37 +0000

    altera a cor do botão back to app do lilas para o verde geral

--- src/app/legal/layout.tsx
+++ src/app/legal/layout.tsx
@@ -42,7 +42,7 @@ const Button = ({ asChild, ...props }: { asChild?: boolean, variant: 'outline', 
   const Comp = asChild ? 'div' : 'button';
   return (
     <Comp
-      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-primary hover:text-primary-foreground h-10 px-4 py-2"
     >
       {props.children}
     </Comp>
