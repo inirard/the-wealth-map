082f2ced092dd75f4ca77dda7855d285d55b92a9
Inira Delgado <iniradelgado@gmail.com>
2025-07-30 17:28:49 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,19 @@
 
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
-// TODO: Cole a sua configuração do Firebase aqui
+// Configuração da sua aplicação web no Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:1234567890abcdef123456"
+  apiKey: "AIzaSyBbPGASGp4pjtC54zWJSevwjsKvDuWTlys",
+  authDomain: "the-wealth-map.firebaseapp.com",
+  projectId: "the-wealth-map",
+  storageBucket: "the-wealth-map.appspot.com",
+  messagingSenderId: "902521497605",
+  appId: "1:902521497605:web:82034a742bf20bce51d46c",
+  measurementId: "G-PQYX7YD59E"
 };
 
-// Inicializar a Firebase App
+
+// Inicializar a Firebase App de forma segura (evita reinicialização)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export { app };
