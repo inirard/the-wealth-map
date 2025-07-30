08dcc6143e04d76ce61fc3d1bd796b8ebb719766
Inira Delgado <iniradelgado@gmail.com>
2025-07-30 23:08:03 +0000

    o icon do perfil precisa de ter mais espaço em relação ao card de bem vi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -29,7 +29,7 @@ export default function DashboardLayout({
                 <UserProfile />
             </div>
         </header>
-        <div className="p-4 sm:p-6 lg:p-8 pt-0 sm:pt-0 lg:pt-0">
+        <div className="p-4 sm:p-6 lg:p-8">
             {children}
         </div>
       </SidebarInset>
