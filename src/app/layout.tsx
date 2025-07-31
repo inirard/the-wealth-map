0d8469450419fabef05d5395e0a29fda10c64463
Inira Delgado <iniradelgado@gmail.com>
2025-07-31 22:25:44 +0000

    na seção dos registos temos registros de hoje entretanto no lançamento a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,7 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" type="image/svg+xml" href={faviconDataUrl} />
+        <link rel="icon" href="data:," />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className="font-body antialiased">
