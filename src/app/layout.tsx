cac2dbb455822d017b1470e78d4e9179bc69155b
Inira Delgado <iniradelgado@gmail.com>
2025-08-02 19:38:25 +0000

    o problema de o ícone do PWA não ser reconhecido está em dois pontos pri

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,11 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning className={`${poppins.variable}`}>
-      <head />
+       <head>
+        <link rel="icon" href="/icons/icon-192x192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/icons/icon-512x512.png" sizes="512x512" type="image/png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      </head>
       <body>
         <I18nProvider>
           {children}
