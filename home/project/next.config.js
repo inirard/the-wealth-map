6e0e21838028280272354fe0b6b2b9331a6edb9a
Inira Delgado <iniradelgado@gmail.com>
2025-08-05 23:24:28 +0000

    sim avançar com cautela para não alterar outras funcionalidades alterar

---
+++ home/project/next.config.js
@@ -0,0 +1,31 @@
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
