e43c98f61ee56bd1a3a759d1ce6066221ff31fa5
Inira Delgado <iniradelgado@gmail.com>
2025-08-06 02:49:58 +0000

    é suposto aparecer instalar pagina do app ou apenas instalar app? os ico

---
+++ home/project/src/app/layout.tsx
@@ -0,0 +1,57 @@
+
+import type { Metadata, Viewport } from 'next';
+import './globals.css';
+import './print.css';
+import { Toaster } from "@/components/ui/toaster";
+import { I18nProvider } from '@/hooks/use-i18n';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['300', '400', '500', '600', '700']
+});
+
+export const metadata: Metadata = {
+  title: 'The Wealth Map | Your Interactive Financial Planner',
+  description: 'Navigate your journey to financial freedom. The Wealth Map is a modern, interactive digital planner to help you set goals, track expenses, and build wealth.',
+  manifest: '/manifest.json',
+  applicationName: "The Wealth Map",
+  appleWebApp: {
+    capable: true,
+    title: "The Wealth Map",
+    statusBarStyle: "default",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: {
+    icon: '/icon.svg',
+    shortcut: '/icon.svg',
+    apple: '/apple-icon.png',
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#007C7C',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="pt-PT" suppressHydrationWarning className={`${poppins.variable}`}>
+       <head />
+      <body>
+        <I18nProvider>
+          {children}
+          <Toaster />
+        </I18nProvider>
+      </body>
+    </html>
+  );
+}
